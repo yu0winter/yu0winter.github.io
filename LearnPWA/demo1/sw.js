865a9df89fdfaa3823585625202d0ec1ddae8f57
牛玉龙 <niuyulong@jd.com>
2018-04-16 17:29:23 +0800

    LearnPWA:测试

---
+++ LearnPWA/demo1/sw.js
@@ -0,0 +1,29 @@
+var cacheStorageKey = 'minimal-pwa-1'
+
+var cacheList = [
+  '/',
+  "index.html",
+  "144x144.png",
+]
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll([
+      'index.html'
+    ]))
+  )
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  )
+})
